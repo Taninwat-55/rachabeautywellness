2142f3c34954fe46e1220c54d96d847983305ff4
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-06-27 19:35:58 +0200

    modified backend and email feature

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ app.use(express.json());
 
 app.use('/api/contact', contactRoutes);
 
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
