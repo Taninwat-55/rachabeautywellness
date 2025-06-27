0b876590060a94cd225af79990077b73b4164648
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-06-27 18:52:19 +0200

    fix backend for production deployment

--- server/server.js
+++ server/server.js
@@ -5,17 +5,20 @@ import contactRoutes from './routes/contact.js';
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173'],
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 app.use('/api/contact', contactRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
