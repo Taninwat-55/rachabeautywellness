0ff74e6dcbebdc9ace104f8ecfe0ffa94d695225
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-06-27 19:18:44 +0200

    Fix contact form for getting email

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -32,7 +32,8 @@ function ContactForm() {
     setStatus('loading'); // Set status to loading
 
     try {
-      const res = await fetch('http://localhost:3001/api/contact', {
+      // const res = await fetch('http://localhost:3001/api/contact', {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
